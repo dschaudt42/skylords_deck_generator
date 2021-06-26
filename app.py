0b47edef7d5a24b253374c8726e841d2d7e0eeeb
Metagross31 <Stephan.H1995@web.de>
2021-06-26 15:24:03 +0200

    Updates app.py to be PEP conform
    This includes a few changes, including:
    - Adding whitespace around operators and commas
    - Not shadowing global variable names inside functions
    - Using `cond is True` instead of `cond == True`
    - Some more whitespace changes

--- app.py
+++ app.py
@@ -5,8 +5,6 @@ import numpy as np
 import pandas as pd
 import random
 
-
-
 """
 # Battleforge Reborn Deck Randomizer
 Here starts the fun!!!!
@@ -28,17 +26,16 @@ min_t2_spells = 1
 min_t3_spells = 1
 min_t4_spells = 1
 
-
 df = pd.read_csv('./data/card_db.csv')
 
 colors = ['Nature', 'Fire', 'Frost', 'Shadow']
-primary_colors = random.sample(colors,k=random.randint(1,2))
+primary_colors = random.sample(colors, k=random.randint(1, 2))
 
 resource_orbs = []
 if len(primary_colors) == 1:
-    resource_orbs = primary_colors*4
+    resource_orbs = primary_colors * 4
 else:
-    resource_orbs = random.choices(primary_colors,k=4)
+    resource_orbs = random.choices(primary_colors, k=4)
 
 
 def color_count_dict(orbs):
@@ -65,35 +62,34 @@ def color_count_dict(orbs):
     return color_dict
 
 
-def is_playable(card_orbs,resource_orbs):
-    is_playable = True
+def is_playable(card_orbs, resource_orbs_):
+    is_playable_ = True
     card_count = color_count_dict(card_orbs)
 
-    for key,value in card_count.items():
-        if color_count_dict(resource_orbs[:len(card_orbs)])[key] < value:
-            is_playable = False
-    return is_playable
+    for key, value in card_count.items():
+        if color_count_dict(resource_orbs_[:len(card_orbs)])[key] < value:
+            is_playable_ = False
+    return is_playable_
 
 
 df['orb_list'] = df['orbs'].str.split(',').tolist()
 df['is_playable'] = df['orb_list'].apply(is_playable, args=([resource_orbs]))
-df_playable = df[df['is_playable']==True]
+df_playable = df[df['is_playable'] is True]
 
+units_filt_t1 = (df_playable['orbsamount'] == 1) & (df_playable['type'] == 'Unit')
+units_filt_t2 = (df_playable['orbsamount'] == 2) & (df_playable['type'] == 'Unit')
+units_filt_t3 = (df_playable['orbsamount'] == 3) & (df_playable['type'] == 'Unit')
+units_filt_t4 = (df_playable['orbsamount'] == 4) & (df_playable['type'] == 'Unit')
 
-units_filt_t1 = (df_playable['orbsamount']==1) & (df_playable['type']=='Unit')
-units_filt_t2 = (df_playable['orbsamount']==2) & (df_playable['type']=='Unit')
-units_filt_t3 = (df_playable['orbsamount']==3) & (df_playable['type']=='Unit')
-units_filt_t4 = (df_playable['orbsamount']==4) & (df_playable['type']=='Unit')
+buildings_filt_t1 = (df_playable['orbsamount'] == 1) & (df_playable['type'] == 'Building')
+buildings_filt_t2 = (df_playable['orbsamount'] == 2) & (df_playable['type'] == 'Building')
+buildings_filt_t3 = (df_playable['orbsamount'] == 3) & (df_playable['type'] == 'Building')
+buildings_filt_t4 = (df_playable['orbsamount'] == 4) & (df_playable['type'] == 'Building')
 
-buildings_filt_t1 = (df_playable['orbsamount']==1) & (df_playable['type']=='Building')
-buildings_filt_t2 = (df_playable['orbsamount']==2) & (df_playable['type']=='Building')
-buildings_filt_t3 = (df_playable['orbsamount']==3) & (df_playable['type']=='Building')
-buildings_filt_t4 = (df_playable['orbsamount']==4) & (df_playable['type']=='Building')
-
-spells_filt_t1 = (df_playable['orbsamount']==1) & (df_playable['type']=='Spell')
-spells_filt_t2 = (df_playable['orbsamount']==2) & (df_playable['type']=='Spell')
-spells_filt_t3 = (df_playable['orbsamount']==3) & (df_playable['type']=='Spell')
-spells_filt_t4 = (df_playable['orbsamount']==4) & (df_playable['type']=='Spell')
+spells_filt_t1 = (df_playable['orbsamount'] == 1) & (df_playable['type'] == 'Spell')
+spells_filt_t2 = (df_playable['orbsamount'] == 2) & (df_playable['type'] == 'Spell')
+spells_filt_t3 = (df_playable['orbsamount'] == 3) & (df_playable['type'] == 'Spell')
+spells_filt_t4 = (df_playable['orbsamount'] == 4) & (df_playable['type'] == 'Spell')
 
 final_deck = pd.DataFrame()
 
@@ -116,9 +112,9 @@ random_card_amount = 20 - final_deck.shape[0]
 random_filler_cards = df_playable[~df_playable.index.isin(final_deck.index)].sample(random_card_amount)
 final_deck = final_deck.append(random_filler_cards)
 
-
 st.write(resource_orbs)
 
-st.dataframe(final_deck.sort_values(by=['orbsamount'])[['name','faction','type','orbsamount']], width=600, height=550)
+st.dataframe(final_deck.sort_values(by=['orbsamount'])[['name', 'faction', 'type', 'orbsamount']], width=600,
+             height=550)
 
 result = st.button("Generate Deck!")
