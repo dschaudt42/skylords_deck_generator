af9adccd23e198fab221063151a18e30bd79a381
Metagross31 <Stephan.H1995@web.de>
2021-09-09 14:41:10 +0200

    Implements PvE and PvP deck buttons
    In this commit a second button has been added for PvP decks. Clicking it
    will generate a deck with different parameters, which thus far are
    hard-coded.
    In order to make this possible, the actual deck generation has been
    wrapped into the functions gen_pve_deck() and gen_pvp_deck()
    respectively. They are then called and the results are written using the
    function write_deck(). The main() function now only contains two
    buttons, each of which will call write_deck() with the parameters
    according to the mode specified on the button.

--- app.py
+++ app.py
@@ -9,21 +9,42 @@ import random
 Here starts the fun!!!!
 """
 
-def main():
+colors = ['Nature', 'Fire', 'Frost', 'Shadow']
+
+
+def color_count_dict(orbs):
+    return {color: orbs.count(color) for color in colors}
+
+
+def is_playable(card_orbs, resource_orbs_):
+    """
+    Determines, weather a card with requirements `card_orbs` is playable,
+    when the orbs listed in`resource_orbs_` are built.
+    :param card_orbs: List of strings, which can be "Nature", "Fire", "Frost" or "Shadow"
+    :param resource_orbs_: List of strings, which can be "Nature", "Fire", "Frost" or "Shadow"
+    :return: Boolean value
+    """
+    card_count = color_count_dict(card_orbs)
+
+    for key, value in card_count.items():
+        if color_count_dict(resource_orbs_[:len(card_orbs)])[key] < value:
+            return False
+    return True
+
+
+def gen_deck(min_tn_units, min_tn_buildings, min_tn_spells, max_n_colors=2, min_n_colors=1):
+    """
+    Generates a random deck fitting to the parameters given.
+    :param min_tn_units: List of integers, which determine how many units each tier must include
+    :param min_tn_buildings: List of integers, which determine how many buildings each tier must include
+    :param min_tn_spells: List of integers, which determine how many spells each tier must include
+    :param max_n_colors: Integer, which gives the maximum amount of colors that can be present in a deck
+    :param min_n_colors: Integer, which gives the minimum amount of colors that can be present in a deck
+    :return: resource_orbs, final_deck - a tuple which has the picked orb colors and the generated deck as its elements.
+    """
     df = pd.read_csv('./data/card_db.csv')
 
-    # Parameters
-    # Minimum number of units/buildings/spells to put into tier n
-    min_tn_units = [2, 2, 2, 2]
-    min_tn_buildings = [1, 1, 1, 1]
-    min_tn_spells = [1, 1, 1, 1]
-
-    # Max/min number of different colours to put into the deck
-    max_n_colors = 2
-    min_n_colors = 1
-
     # The colors of the orbs are chosen
-    colors = ['Nature', 'Fire', 'Frost', 'Shadow']
     primary_colors = random.sample(colors, k=random.randint(min_n_colors, max_n_colors))
 
     resource_orbs = []
@@ -31,24 +52,6 @@ def main():
         resource_orbs = primary_colors * 4
     else:
         resource_orbs = random.choices(primary_colors, k=4)
-
-    def color_count_dict(orbs):
-        return {color: orbs.count(color) for color in colors}
-
-    def is_playable(card_orbs, resource_orbs_):
-        """
-        Determines, weather a card with requirements `card_orbs` is playable,
-        when the orbs listed in`resource_orbs_` are built.
-        :param card_orbs: List of strings, which can be "Nature", "Fire", "Frost" or "Shadow"
-        :param resource_orbs_: List of strings, which can be "Nature", "Fire", "Frost" or "Shadow"
-        :return: Boolean value
-        """
-        card_count = color_count_dict(card_orbs)
-
-        for key, value in card_count.items():
-            if color_count_dict(resource_orbs_[:len(card_orbs)])[key] < value:
-                return False
-        return True
 
     df['orb_list'] = df['orbs'].str.split(',').tolist()
     df['is_playable'] = df['orb_list'].apply(is_playable, args=([resource_orbs]))
@@ -71,12 +74,64 @@ def main():
     random_filler_cards = df_playable[~df_playable.index.isin(final_deck.index)].sample(random_card_amount)
     final_deck = final_deck.append(random_filler_cards)
 
-    st.write(resource_orbs)
+    return resource_orbs, final_deck
 
+
+def gen_pve_deck():
+    """
+    Generates a deck with parameters fitting for a PvE Deck. These are the parameters used in the program previously.
+    :return: resource_orbs, final_deck - a tuple which has the picked orb colors and the generated deck as its elements.
+    """
+    min_tn_units = (2, 2, 2, 2)
+    min_tn_buildings = (1, 1, 1, 1)
+    min_tn_spells = (1, 1, 1, 1)
+
+    max_n_colors = 2
+    min_n_colors = 1
+
+    return gen_deck(min_tn_units, min_tn_buildings, min_tn_spells, max_n_colors, min_n_colors)
+
+
+def gen_pvp_deck():
+    """
+    Generates a deck with parameters fitting for a PvP Deck. These parameters have been chosen by me kind of arbitrarily
+    but in a way, that seemed fitting for a PvP Deck.
+    :return: resource_orbs, final_deck - a tuple which has the picked orb colors and the generated deck as its elements.
+    """
+    min_tn_units = (2, 2, 2, 0)
+    min_tn_buildings = (1, 1, 0, 0)
+    min_tn_spells = (1, 1, 1, 0)
+
+    max_n_colors = 3
+    min_n_colors = 1
+
+    return gen_deck(min_tn_units, min_tn_buildings, min_tn_spells, max_n_colors, min_n_colors)
+
+
+def write_deck(gamemode=0):
+    """
+    Generates a deck fitting to the given game mode and writes it to the application
+    :param gamemode: Integer, 0 for PvE, 1 for PvP
+    :return: None
+    """
+    if gamemode == 0:
+        resource_orbs, final_deck = gen_pve_deck()
+    else:
+        resource_orbs, final_deck = gen_pvp_deck()
+
+    st.write(resource_orbs)
     st.dataframe(final_deck.sort_values(by=['orbsamount'])[['name', 'faction', 'type', 'orbsamount']], width=600,
                  height=550)
 
-    result = st.button("Generate Deck!")
+
+def main():
+    # Before clicking anything, the main app only consists of two buttons - one for a PvE deck and one for a PvP deck.
+    # Clicking one of them will generate a deck and write it to the app.
+    if st.button("Generate PvE Deck!"):
+        write_deck(0)
+
+    if st.button("Generate PvP Deck!"):
+        write_deck(1)
 
 
 if __name__ == '__main__':
